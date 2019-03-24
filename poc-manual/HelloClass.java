1b6ab9c07bddea3b6a0eab25fa7042d1291765a1
niels <n.frerichs83@gmail.com>
2019-03-24 23:18:33 +0100

    properties

--- poc-manual/HelloClass.java
+++ poc-manual/HelloClass.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public class HelloClass{
   public static void main(String[] args){
     System.out.println("Hello");
 
     NewsStation newsStation = new NewsStation("Georgia");
     System.out.println(newsStation.broadCast());
+
+    Properties properties = System.getProperties();
+        for (String propertyName : properties.stringPropertyNames()) {
+            System.out.println(propertyName + ": " + System.getProperty(propertyName));
+        }
   }
 }
