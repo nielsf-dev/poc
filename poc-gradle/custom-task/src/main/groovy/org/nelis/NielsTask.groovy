fb5df30b62d78dbdbf09f3960eb049221b5ff3fb
niels <n.frerichs83@gmail.com>
2019-04-17 12:19:39 +0200

    custom gradle task

---
+++ poc-gradle/custom-task/src/main/groovy/org/nelis/NielsTask.groovy
@@ -0,0 +1,13 @@
+package org.gradle
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class NielsTask extends DefaultTask {
+    String greeting = 'King Frerichs aan de macht'
+
+    @TaskAction
+    def greet() {
+        println greeting
+    }
+}
