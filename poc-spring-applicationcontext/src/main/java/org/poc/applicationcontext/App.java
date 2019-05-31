2274e07b5fec7e69daa1b5ed0d4fe24bfb732572
niels <n.frerichs83@gmail.com>
2019-05-31 17:46:33 +0200

    nudan

--- poc-spring-applicationcontext/src/main/java/org/poc/applicationcontext/App.java
+++ poc-spring-applicationcontext/src/main/java/org/poc/applicationcontext/App.java
@@ -6,17 +6,27 @@ package org.poc.applicationcontext;
 import org.poc.applicationcontext.func.SuperService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         SuperService bean = ctx.getBean(SuperService.class);
 
-        System.out.println(bean.doSomethingAwesome());
+        Locale aDefault = Locale.getDefault();
+        Locale.setDefault(new Locale("nl","NL"));
+        Locale aDefault2 = Locale.getDefault();
+
+        ResourceBundle messages = ResourceBundle.getBundle("messages");
+        System.out.println(messages.getString("who_text"));
     }
 }
--- poc-spring-applicationcontext/src/main/java/org/poc/applicationcontext/AppConfig.java
+++ poc-spring-applicationcontext/src/main/java/org/poc/applicationcontext/AppConfig.java
@@ -1,9 +1,18 @@
 package org.poc.applicationcontext;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 @ComponentScan("org.poc.applicationcontext.func")
 public class AppConfig {
+
+    @Bean
+    public ResourceBundleMessageSource messageSource(){
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        return resourceBundleMessageSource;
+    }
+
 }
