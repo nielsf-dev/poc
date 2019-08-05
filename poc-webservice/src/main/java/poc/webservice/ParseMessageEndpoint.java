1831c9e865b294fcb1be95778c8c2eaf3b9530ce
niels <n.frerichs83@gmail.com>
2019-08-05 17:41:29 +0200

    semi werkende webservice

---
+++ poc-webservice/src/main/java/poc/webservice/ParseMessageEndpoint.java
@@ -0,0 +1,10 @@
+package poc.webservice;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+
+@Endpoint
+public class ParseMessageEndpoint {
+    //"http://www.visi.nl/schemas/soap/version-1.0"
+
+
+}
--- poc-webservice/src/main/java/poc/webservice/WebserviceApplication.java
+++ poc-webservice/src/main/java/poc/webservice/WebserviceApplication.java
@@ -3,18 +3,51 @@ package poc.webservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 @SpringBootApplication
+@EnableWs
 public class WebserviceApplication {
 
     @Bean
     public CommandLineRunner commandLineRunner(WebserviceClient webServiceClient) {
         return args -> {
-            webServiceClient.callParseMessage();
+          //  webServiceClient.callParseMessage();
             System.out.println("werkt");
             System.out.println("werkt");
         };
+    }
+
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+
+    @Bean(name = "countries")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema parseMessage) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("ParseMessagePort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://www.visi.nl/schemas/soap/version-1.0");
+        wsdl11Definition.setSchema(parseMessage);
+
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema parseMessage() {
+        return new SimpleXsdSchema(new ClassPathResource("parseMessage.xsd"));
     }
 
     public static void main(String[] args) {
