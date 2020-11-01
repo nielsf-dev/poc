2dbeee82ae4d4fdb2e28447ed68e28ed8ba63b83
Niels Frerichs <n.frerichs83@gmail.com>
2020-11-01 02:04:17 +0100

    alles op 1 grote hoop hotseeee

--- poc-concurrency/heavy-load/src/main/java/org/poc/heavyload/Runner.java
+++ poc-concurrency/heavy-load/src/main/java/org/poc/heavyload/Runner.java
@@ -7,12 +7,12 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.Buffer;
-import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
@@ -21,34 +21,34 @@ import java.util.concurrent.atomic.AtomicLong;
 public class Runner implements CommandLineRunner {
     Logger logger = LoggerFactory.getLogger(Runner.class);
 
-    static AtomicLong connections = new AtomicLong(0);
-    static AtomicLong reads = new AtomicLong(0);
-    static AtomicLong readTimeTotal = new AtomicLong(0);
-    static AtomicLong writes = new AtomicLong(0);
-    static AtomicLong writeTimeTotal = new AtomicLong(0);
+    long connections = 0;
+
+    static AtomicLong requests = new AtomicLong(0);
+    static AtomicLong requestTimeTotal = new AtomicLong(0);
     long totalElapsed = 0;
 
     static AtomicBoolean shutDown = new AtomicBoolean(false);
 
     @Override
     public void run(String... args) throws Exception {
-        ExecutorService executorService = Executors.newFixedThreadPool(30000);
+        int nConnections = Integer.parseInt(args[0]);
+        ExecutorService executorService = Executors.newFixedThreadPool(nConnections);
 
-        final Socket[] sockets = new Socket[Integer.parseInt(args[0])]; // Create 3000 connections
+        final Socket[] sockets = new Socket[nConnections]; // Create n connections
         for (int x = 0; x < sockets.length; x++) {
             try {
-                long l = connections.incrementAndGet();
+
 
                 long start = System.nanoTime();
                 Socket socket = new Socket(args[1], Integer.parseInt(args[2]));
                 long elapsed = System.nanoTime() - start;
 
-                System.out.println("Connected " + l + " took " + elapsed/1000000 + "ms");
+                System.out.println("Connected " + ++connections + " took " + elapsed/1000000 + "ms");
                 totalElapsed += elapsed;
 
                 sockets[x] = socket;
-                executorService.submit(() -> handleSocket(socket, l));
-                
+                Future<?> future = executorService.submit(() -> handleSocket(socket, connections));
+
             } catch (final IOException e) {
                 System.err.println(e);
             }
@@ -56,40 +56,44 @@ public class Runner implements CommandLineRunner {
 
         System.out.println("Connected " + sockets.length + " sockets. Average: " + (totalElapsed/sockets.length)/1000000 + "ms");
 
-        long readsNow = reads.longValue();
-        System.out.println("ReadTime total = " + readTimeTotal);
-        System.out.println(readsNow + " reads. Average: " + (readTimeTotal.get()/ readsNow)/1000000 + "ms");
+        while(true) {
+            long writesnow = requests.longValue();
+            System.out.println(writesnow + " Requests. Average: " + (requestTimeTotal.get() / writesnow) / 1000000 + "ms");
 
-        long writesnow = writes.longValue();
-        System.out.println("WriteTime total = " + writeTimeTotal);
-        System.out.println(writesnow + " writes. Average: " + (writeTimeTotal.get()/ writesnow)/1000000 + "ms");
+            requests.set(0);
+            requestTimeTotal.set(0);
+            Thread.sleep(1000 * 3);
+        }
 
-        shutDown.set(true);
-        executorService.shutdown();
-        executorService.awaitTermination(55, TimeUnit.MILLISECONDS);
+//        shutDown.set(true);
+//        executorService.shutdown();
+//        executorService.awaitTermination(55, TimeUnit.MILLISECONDS);
     }
 
     private void handleSocket(Socket socket, long connection)  {
         int error = 0;
         while(!shutDown.get()){
             try {
-                long startWrite = System.nanoTime();
-                String s1 = "King Niels aan de macht super cool kickken HATERS GONNA GATE THE END";
-                byte[] bytesTHatGoIn = s1.getBytes();
-                socket.getOutputStream().write(bytesTHatGoIn);
-                writeTimeTotal.addAndGet(System.nanoTime() - startWrite);
-                writes.incrementAndGet();
-
-                long startRead = System.nanoTime();
+                OutputStream outputStream = socket.getOutputStream();
                 InputStream inputStream = socket.getInputStream();
-                byte[] buffer = new byte[s1.length()];
-                byte read = (byte) inputStream.read(buffer,0,buffer.length);
-                readTimeTotal.addAndGet(System.nanoTime() - startRead);
-                reads.incrementAndGet();
 
-                byte[] readBytes = Arrays.copyOf(buffer, read);
-                String readFromSocket = new String(readBytes);
-                logger.info("Read from socket on connection " + connection + ": " + readFromSocket);
+                long startWrite = System.nanoTime();
+                String s1 = "King Niels aan de macht super cool kickken HATERS GONNA GATE THE END\n";
+                byte[] bytesTHatGoIn = s1.getBytes();
+                outputStream.write(bytesTHatGoIn);
+
+                byte[] buffer = new byte[s1.length() + 33];
+                byte read = (byte) inputStream.read(buffer,0,buffer.length);
+                if(read == -1)
+                    System.out.println("viel niks te lezen na een write op connection " + connection);
+                else {
+                    requestTimeTotal.addAndGet(System.nanoTime() - startWrite);
+                    requests.incrementAndGet();
+
+                    byte[] readBytes = Arrays.copyOf(buffer, read);
+                    String readFromSocket = new String(readBytes);
+                    logger.info("Read from socket on connection " + connection + ": " + readFromSocket);
+                }
 
                 Thread.sleep(50);
             }
--- poc-concurrency/src/test/java/nio/ThreadedBlockingServer.java
+++ poc-concurrency/src/test/java/nio/ThreadedBlockingServer.java
@@ -1,6 +1,8 @@
 package nio;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,11 +10,18 @@ import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class ThreadedBlockingServer {
 
     static AtomicLong connections = new AtomicLong(0);
+    static Logger logger = LoggerFactory.getLogger(ThreadedBlockingServer.class);
+
+    public ThreadedBlockingServer() throws NoSuchAlgorithmException {
+    }
 
     @Test
     public void test() throws IOException {
@@ -23,7 +32,7 @@ public class ThreadedBlockingServer {
                 System.out.println("accepting..");
                 final Socket socket = serverSocket.accept(); // Blocking until someone connects
                 System.out.println("..after accept");
-             //   System.out.println("Incoming connection " + connections);
+                //   System.out.println("Incoming connection " + connections);
 
                 new Thread(() -> handle(socket)).start();
             }
@@ -39,22 +48,79 @@ public class ThreadedBlockingServer {
     private static void handle(final Socket socket) {
         long l = connections.incrementAndGet();
         System.out.println("Handling connection " + l);
-
-        try (InputStream inputStream = socket.getInputStream();
-             OutputStream outputStream = socket.getOutputStream()) {
-            int data;
-            while ((data = inputStream.read()) != -1) { // -1 -> end of stream
-                // Data read from socket
-
-                // Operation performed
-                data = Character.isLetter(data) ? invertCase(data) : data;
-
-                // Changed output written back to socket
-                outputStream.write(data);
-            }
-        } catch (final IOException e) {
-            throw new UncheckedIOException(e);
+        InputStream inputStream = null;
+        try {
+            inputStream = socket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        OutputStream outputStream = null;
+        try {
+            outputStream = socket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while(true) {
+            try  {
+                String input = readString(inputStream);
+                if(!input.isEmpty() && !input.isBlank()) {
+                    MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+                    byte[] digest = messageDigest.digest(input.getBytes());
+                    String md5 = toHexString(digest);
+                    String output = input + " " + md5;
+
+                    outputStream.write(output.getBytes());
+                }
+                else
+                    logger.error("Ongeldige string gelezen: " + input);
+
+
+//            int data;
+//            while ((data = inputStream.read()) != -1) { // -1 -> end of stream
+//                // Data read from socket
+//
+//                // Operation performed
+//                data = Character.isLetter(data) ? invertCase(data) : data;
+//
+//                // Changed output written back to socket
+//                outputStream.write(data);
+//            }
+            } catch (Exception e) {
+                logger.error("Error in handlen van socket", e);
+            }
+        }
+    }
+
+    public static String toHexString(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(0xFF & bytes[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
+
+    private static String readString(InputStream inputStream) throws IOException {
+        int data;
+        StringBuilder sb = new StringBuilder();
+        while ((data = inputStream.read()) != -1) { // -1 -> end of stream
+            // Data read from socket
+
+            // Operation performed
+            char chr = (char) data;
+            sb.append(chr);
+
+            if(chr == '\n')
+                break;
+        }
+
+        return sb.toString();
     }
 
     private static int invertCase(final int data) {
