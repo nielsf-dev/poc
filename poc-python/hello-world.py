50d6b7896f5ac1e723e9802d5daae670ffacd1b9
Niels Frerichs <n.frerichs83@gmail.com>
2021-02-17 02:01:56 +0100

    dik gaan

--- poc-python/hello-world.py
+++ poc-python/hello-world.py
@@ -1,1 +1,19 @@
-print('Hello, world!')
+import subprocess
+
+
+# useless_cat_call = subprocess.run(["java", "--version"], stdout=subprocess.PIPE, text=True)
+# print(useless_cat_call.stdout)
+
+def vet_dik_gaan():
+    print("doen")
+    print("durven")
+    print("yooooolo")
+
+
+vet_dik_gaan()
+useless_cat_call = subprocess.run(["java", "--version"])
+print(useless_cat_call.stdout)
+
+some_input = input("Please enter an integer: ")
+x = int(some_input)
+print('Hello, world!' + str(x))
--- poc-react/node-consoleapp/consoleapp.js
+++ poc-react/node-consoleapp/consoleapp.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 console.log('yolo');
+console.error()
 
 process.argv.forEach((val, index) => {
   console.log(`${index}: ${val}`);
