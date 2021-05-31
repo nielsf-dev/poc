6726cce8cb976284a6c9e24834d38720648e7680
Niels <n.frerichs83@gmail.com>
2021-05-31 22:36:45 +0200

    action jacksion

--- poc-netcore/net5aspcore/Program.cs
+++ poc-netcore/net5aspcore/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,12 +14,22 @@ namespace net5aspcore
     {
         public static void Main(string[] args)
         {
+            var myBuilder = new MyBuilder();
+            myBuilder.DoInterfaceStuff(interf =>
+                {
+                    interf.AddStuff("stuff");
+                })
+                .DoOtherStuff();
+
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(stinkerd());
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var defaultBuilder = Host.CreateDefaultBuilder(args);
+            return defaultBuilder;
+        }
+        //    .ConfigureWebHostDefaults(stinkerd());
 
         private static Action<IWebHostBuilder> stinkerd()
         {
@@ -27,5 +38,33 @@ namespace net5aspcore
                 webBuilder.UseStartup<Startup>();
             };
         }
+    }
+
+    public class MyBuilder
+    {
+        public MyBuilder DoInterfaceStuff(Action<MyInterface> myAction)
+        {
+            var myImp = new MyImplementation();
+            myAction(myImp);
+            return this;
+        }
+
+        public MyBuilder DoOtherStuff()
+        {
+            return this;
+        }
+    }
+
+    public class MyImplementation : MyInterface
+    {
+        public void AddStuff(string stuff)
+        {
+            Debug.WriteLine("Doing stuffff!");
+        }
+    }
+
+    public interface MyInterface
+    {
+        void AddStuff(string stuff);
     }
 }
