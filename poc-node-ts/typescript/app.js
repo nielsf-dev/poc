c13dd632a7fffdc54a6591c7820b84551914f173
Niels <niels@bakkerspees.nl>
2021-10-22 15:54:13 +0200

    basis html

--- poc-node-ts/typescript/app.js
+++ poc-node-ts/typescript/app.js
@@ -3,3 +3,5 @@ var message2 = 'Hello, World!';
 console.log(message2);
 var message3 = 'foo-bar';
 console.log(camelCase(message3));
+var mydiv = document.getElementById('changeme');
+mydiv.innerText = message3;
