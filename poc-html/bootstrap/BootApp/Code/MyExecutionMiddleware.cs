1b480cb16053e6e2e75dfe508b0ad1a786b25f57
Niels Frerichs <n.frerichs83@gmail.com>
2022-01-01 20:13:48 +0100

    interesting linker middleware

---
+++ poc-html/bootstrap/BootApp/Code/MyExecutionMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace BootApp.Code;
+
+public class MyExecutionMiddleware
+{
+    private readonly RequestDelegate next;
+
+    public MyExecutionMiddleware(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context, BootAppDbContext dbContext)
+    {
+        await dbContext.Persons.ToListAsync();
+        await Task.Delay(2);
+        await next(context);
+    }
+}
--- poc-html/bootstrap/BootApp/Startup.cs
+++ poc-html/bootstrap/BootApp/Startup.cs
@@ -52,39 +52,78 @@ namespace BootApp
               app.UseStaticFiles();
              
             app.UseRouting();
+            
+          Func<HttpContext, RequestDelegate, Task> myExecutionNextMiddlewareFunction = async (context, next) =>
+          {
+              var currentEndpoint = context.GetEndpoint();
 
+              if (currentEndpoint is null)
+              {
+                  await next(context);
+                  return;
+              }
 
-            app.Use(async (context, next) =>
-            {
-                var currentEndpoint = context.GetEndpoint();
+              Console.WriteLine($"Endpoint: {currentEndpoint.DisplayName}");
 
-                if (currentEndpoint is null)
-                {
-                    await next(context);
-                    return;
-                }
+              if (currentEndpoint is RouteEndpoint routeEndpoint)
+              {
+                  Console.WriteLine($"  - Route Pattern: {routeEndpoint.RoutePattern.RawText}");
+              }
 
-                Console.WriteLine($"Endpoint: {currentEndpoint.DisplayName}");
+              foreach (var endpointMetadata in currentEndpoint.Metadata)
+              {
+                  Console.WriteLine($"  - Metadata: {endpointMetadata}");
+              }
 
-                if (currentEndpoint is RouteEndpoint routeEndpoint)
-                {
-                    Console.WriteLine($"  - Route Pattern: {routeEndpoint.RoutePattern.RawText}");
-                }
+              await next(context);
+          };
 
-                foreach (var endpointMetadata in currentEndpoint.Metadata)
-                {
-                    Console.WriteLine($"  - Metadata: {endpointMetadata}");
-                }
+          // Dit gaat dus niet omdat input delegate niet beschikbaar is
+          // RequestDelegate myWrapper = context =>
+          // {
+          //     myExecutionNextMiddlewareFunction(context, inputDelegate);
+          // };
 
-                await next(context);
-            });
+          Func<RequestDelegate, RequestDelegate> myLinkerFunction = inputDelegate =>
+          {
+              //input delegate komt hier uit, de "next" in onze middleware function, en deze is dus yet to be decided
+              RequestDelegate outputDelegate = context =>
+              {
+                  return myExecutionNextMiddlewareFunction(context, inputDelegate);
+              };
+
+              // wat voert de delegate uit die gereturned word?
+              // F#1 onze middleware function, met als input de yet to be decided
+
+              return outputDelegate;
+          };
+
+          app.Use(myLinkerFunction);
+
+          // stel dit is de laatste, dan voert de applicationbuilder deze uit met als input een "throw error" request delegate.
+
+          // de vorige middleware, krijgt dan als next F#1
+
+            returnInt n = i => 5 * i;
+            var result = n(10);
+
+            app.UseMiddleware<MyExecutionMiddleware>();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
-            }); 
-         
+            });
+        }
 
+        public delegate int returnInt(int n);
+
+        private RequestDelegate myRequestFunction(RequestDelegate input)
+        {
+            return context =>
+            {
+                Console.WriteLine(context.Connection);
+                return Task.CompletedTask;
+            };
         }
     }
 }
